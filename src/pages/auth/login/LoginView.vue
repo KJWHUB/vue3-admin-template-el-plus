e763475fce0d4df35ce1e6d792616045691827e9
KJWHUB <126055193+KJWHUB@users.noreply.github.com>
2024-07-10 15:57:31 +0900

    fsd pattern 02 (page)[0]

---
+++ src/pages/auth/login/LoginView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import LoginCard from '@/components/login-card/LoginCard.vue'
+</script>
+
+<template>
+  <main class="login-view">
+    <LoginCard style="width: 40%" />
+  </main>
+</template>
+
+<style scoped>
+.login-view {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  min-height: 100vh;
+}
+</style>
---
+++ src/pages/contents/dashboard/DashboardView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <el-card>
+    <h1>Dashboard</h1>
+  </el-card>
+</template>
+
+<style scoped></style>
